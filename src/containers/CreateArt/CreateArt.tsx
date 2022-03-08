39f101e362a6cfe3ae4a90168e3f931389c703b2
Andrey Andronov <me@andronov.io>
2022-03-09 00:10:39 +0300

    Init editor art iframe

--- src/containers/CreateArt/CreateArt.tsx
+++ src/containers/CreateArt/CreateArt.tsx
@@ -1,10 +1,37 @@
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
+import { useEffect, useRef, useState } from 'react';
 
 const CreateArt = () => {
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const refContainer = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (refContainer.current) {
+      const offset = 50;
+      const rect = refContainer.current?.getBoundingClientRect();
+      const height = window.innerHeight - rect.top - offset;
+      const width = rect.width;
+      setSize({ width, height });
+    }
+  }, []);
+
   return (
-    <section>
+    <section className={'h-full'}>
       <div>
         <Breadcrumbs />
+      </div>
+
+      <div ref={refContainer} className={'w-full h-full pt-4'}>
+        {size.width && size.height && (
+          <div
+            style={{
+              width: size.width,
+              height: size.height
+            }}
+          >
+            <iframe width={'100%'} height={'100%'} src={'https://xeditor-dun.vercel.app/'} className={'iframe'} sandbox={'allow-same-origin allow-scripts'} />
+          </div>
+        )}
       </div>
     </section>
   );
